6e82cc0ec25827279780cf15b5257445995da38b
Aleksandr Vinokurov <johannblx@gmail.com>
2024-05-30 20:25:49 +0700

    ui fixes
    added small adaptive for mobile

--- tree-app/src/components/treeNode.js
+++ tree-app/src/components/treeNode.js
@@ -11,33 +11,33 @@ const TreeNode = ({ node, onAdd, onRemove, onEdit }) => {
 
     return (
         <div className="tree-node">
-        {isEditing ? (
-            <input
-            type="text"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            />
-        ) : (
-            <span>{node.name}</span>
-        )}
-        <button className="edit" onClick={handleEdit}>
-            {isEditing ? 'Save' : 'Edit'}
-        </button>
-        <button className="add" onClick={() => onAdd(node.id)}>Add</button>
-        <button className="remove" onClick={() => onRemove(node.id)}>Remove</button>
-        {node.children && (
-            <div>
-            {node.children.map((child) => (
-                <TreeNode
-                key={child.id}
-                node={child}
-                onAdd={onAdd}
-                onRemove={onRemove}
-                onEdit={onEdit}
+            {isEditing ? (
+                <input
+                type="text"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
                 />
-            ))}
-            </div>
-        )}
+            ) : (
+                <span>{node.name}</span>
+            )}
+            <button className="edit" onClick={handleEdit}>
+                {isEditing ? 'Save' : 'Edit'}
+            </button>
+            <button className="add" onClick={() => onAdd(node.id)}>Add</button>
+            <button className="remove" onClick={() => onRemove(node.id)}>Remove</button>
+            {node.children && (
+                <div>
+                {node.children.map((child) => (
+                    <TreeNode
+                    key={child.id}
+                    node={child}
+                    onAdd={onAdd}
+                    onRemove={onRemove}
+                    onEdit={onEdit}
+                    />
+                ))}
+                </div>
+            )}
         </div>
     );
 };
